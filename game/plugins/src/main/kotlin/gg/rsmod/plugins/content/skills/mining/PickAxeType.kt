12b3f050fb0e1903d26a07d01aed6433d04f432c
pitchblac23 <tylermincks@hotmail.com>
2022-09-17 14:44:37 -0500

    Adding Animations
    1. adding mining animations
    will need to fix mining for different animations

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/mining/PickAxeType.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/mining/PickAxeType.kt
@@ -1,22 +1,29 @@
 package gg.rsmod.plugins.content.skills.mining
 
+import gg.rsmod.plugins.api.cfg.Animation
 import gg.rsmod.plugins.api.cfg.Items
 
 /**
  * @author Anthony Loukinas <anthony.loukinas@gmail.com>
  */
 enum class PickaxeType(val item: Int, val level: Int, val animation: Int) {
-    BRONZE(item = Items.BRONZE_PICKAXE, level = 1, animation = 625),
-    IRON(item = Items.IRON_PICKAXE, level = 1, animation = 626),
-    STEEL(item = Items.STEEL_PICKAXE, level = 6, animation = 627),
-    BLACK(item = Items.BLACK_PICKAXE, level = 11, animation = 625),
-    MITHRIL(item = Items.MITHRIL_PICKAXE, level = 21, animation = 629),
-    ADAMANT(item = Items.ADAMANT_PICKAXE, level = 31, animation = 628),
-    RUNE(item = Items.RUNE_PICKAXE, level = 41, animation = 624),
-    // NEED TO ADD GILDED PICKAXES
-    DRAGON(item = Items.DRAGON_PICKAXE, level = 61, animation = 7139),
-    THIRD_AGE(item = Items._3RD_AGE_PICKAXE, level = 61, animation = 7284),
-    INFERNAL(item = Items.INFERNAL_PICKAXE, level = 61, animation = 4483);
+
+    BRONZE(Items.BRONZE_PICKAXE, level = 1, Animation.MINING_BRONZE_PICKAXE),
+    IRON(Items.IRON_PICKAXE, level = 1, Animation.MINING_IRON_PICKAXE),
+    STEEL(Items.STEEL_PICKAXE, level = 6, Animation.MINING_STEEL_PICKAXE),
+    BLACK(Items.BLACK_PICKAXE, level = 11, Animation.MINING_BLACK_PICKAXE),
+    MITHRIL(Items.MITHRIL_PICKAXE, level = 21, Animation.MINING_MITHRIL_PICKAXE),
+    ADAMANT(Items.ADAMANT_PICKAXE, level = 31, Animation.MINING_ADAMANT_PICKAXE),
+    RUNE(Items.RUNE_PICKAXE, level = 41, Animation.MINING_RUNE_PICKAXE),
+    GILDED(Items.GILDED_PICKAXE, level = 41, Animation.MINING_GUILDED_PICKAXE),
+    THIRDAGE(Items._3RD_AGE_PICKAXE, level = 61, Animation.MINING_THIRDAGE_PICKAXE),
+    DRAGON(Items.DRAGON_PICKAXE, level = 61, Animation.MINING_DRAGON_PICKAXE),
+    DRAGON_OR(Items.DRAGON_PICKAXE_OR, level = 61, Animation.MINING_DRAGON_PICKAXE_O),
+    DRAGON_U(Items.DRAGON_PICKAXE_12797, level = 61, Animation.MINING_DRAGON_PICKAXE_U),
+    DRAGON_OR_TRAILBLAZER(Items.DRAGON_PICKAXE_OR_25376, level = 61, 625),
+    INFERNAL(Items.INFERNAL_PICKAXE, level = 61, Animation.MINING_INFERNAL_PICKAXE),
+    INFERNAL_PICKAXE_OR(Items.INFERNAL_PICKAXE_OR, level = 61, 625),
+    CRYSTAL(Items.CRYSTAL_PICKAXE, level = 71, Animation.MINING_CRYSTAL_PICKAXE);
 
     companion object {
         val values = enumValues<PickaxeType>()
